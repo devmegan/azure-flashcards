23dff3f6dc0f77273d7a50901de4633b7b723158
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-04 18:59:00 +0000

    add: handle db questions query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import { useState } from "react";
-import Hero from "./component/Hero";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+
 import db from "./firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
 
+import Hero from "./component/Hero";
+import FlashcardWrapper from "./component/FlashcardWrapper";
+
 export default function App() {
-  const [track, setTrack] = useState('az900');
+  const [track, setTrack] = useState();
+  const [questions, setQuestions] = useState();
 
   const fetchQuestions = async() => {
-    const questions = doc(db, "questions", track);
-    const docSnap = await getDoc(questions);
-    console.log("Document data:", docSnap.data());
+    setQuestions(null);
+    const res = doc(db, "questions", track);
+    const questionData = await getDoc(res);
+    if (questionData.exists() && Object.keys(questionData.data()).length !== 0) {
+      setQuestions(questionData.data());
+    } else {
+      console.log("no data :( sorry ");
+    }
   };
 
-  useState(() => {
-    fetchQuestions();
-  }, []);
-
+  useEffect(() => {
+    if (track) {
+      fetchQuestions();
+    }
+  }, [track]);
 
   return (
-    <div className="container mx-auto" data-theme="aqua">
+    <div data-theme="aqua">
       <Hero track={track} setTrack={setTrack} />
+      {questions && <FlashcardWrapper track={track} questions={questions} />}
     </div>
   );
 }
