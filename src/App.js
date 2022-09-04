ca3a791aa1028cb9fad61ce7edee2d8f130b237f
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-04 09:44:08 +0000

    add: state/setter for selected track

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Hero from "./component/Hero";
 
 export default function App() {
+  const [track, setTrack] = useState('az900');
+
   return (
-    <div data-theme="aqua">
-      <Hero />
+    <div className="container" data-theme="aqua">
+      <Hero track={track} setTrack={setTrack} />
     </div>
   );
 }
