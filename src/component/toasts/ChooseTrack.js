df678e84ced7f74309b9de8ba7cce7240c2fdfdf
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-04 19:00:32 +0000

    add: choose track message

--- src/component/toasts/ChooseTrack.js
+++ src/component/toasts/ChooseTrack.js
@@ -1,5 +1,7 @@
 export default function ChooseTrack() {
     return (
-        <div className="alert shadow-lg alert-sm mb-3 max-w-xs mx-auto">Choose a track.</div> 
+        <div className="alert shadow-lg alert-sm mb-3 max-w-xs flex justify-center mx-auto">
+            Choose a track.
+        </div> 
     );
 }
