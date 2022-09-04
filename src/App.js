91a3563cac7e454869c443470d63ac3afc647673
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-04 11:56:25 +0000

    wip: get track question data from firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import Hero from "./component/Hero";
+import db from "./firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
 
 export default function App() {
   const [track, setTrack] = useState('az900');
 
+  const fetchQuestions = async() => {
+    const questions = doc(db, "questions", track);
+    const docSnap = await getDoc(questions);
+    console.log("Document data:", docSnap.data());
+  };
+
+  useState(() => {
+    fetchQuestions();
+  }, []);
+
+
   return (
-    <div className="container" data-theme="aqua">
+    <div className="container mx-auto" data-theme="aqua">
       <Hero track={track} setTrack={setTrack} />
     </div>
   );
