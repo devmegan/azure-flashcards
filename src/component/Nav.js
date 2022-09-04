2efc3113d8b263ea1821fad43b8560fbdd192580
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-04 16:16:08 +0000

    add: btn group to nav

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,24 +1,27 @@
 export default function Nav({ track, setTrack }) {
     return (
-        <nav className="grid grid-cols-1 sm:grid-cols-3 gap-10 px-10 mt-10 max-w-md mx-auto">
-            <button 
-                className={`btn btn-primary ${track !== 'az900' && 'btn-outline'}`} 
-                onClick={() => setTrack("az900")}
-            >
-                AZ-900
-            </button>
-            <button 
-                className={`btn btn-primary ${track !== 'az204' && 'btn-outline'}`} 
-                onClick={() => setTrack("az204")}
-            >
-                AZ-204
-            </button>
-            <button 
-                className={`btn btn-primary ${track !== 'az400' && 'btn-outline'}`} 
-                onClick={() => setTrack("az400")}
-            >
-                AZ-400 
-            </button>
+        <nav className="px-10 max-w-md mx-auto content-center">
+            <div className="alert shadow-lg alert-sm mb-3 max-w-xs mx-auto">Choose a track.</div>
+            <div className="btn-group flex justify-center">
+                <button 
+                    className={`btn float-none ${track === 'az900' && 'btn-active'}`}
+                    onClick={() => setTrack("az900")}
+                >
+                    AZ-900
+                </button>
+                <button 
+                    className={`btn ${track === 'az204' && 'btn-active'}`}
+                    onClick={() => setTrack("az204")}
+                >
+                    AZ-204
+                </button>
+                <button 
+                    className={`btn ${track === 'az400' && 'btn-active'}`}
+                    onClick={() => setTrack("az400")}
+                >
+                        AZ-400
+                </button>
+            </div>
         </nav>
     );
-  }
+}
