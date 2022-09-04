75506e9018fa546762c33fea011a656c6c495bf8
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-04 11:56:43 +0000

    update: fix hero padding

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -6,7 +6,7 @@ const az400 = "https://docs.microsoft.com/en-us/certifications/exams/az-400";
 
 export default function Hero({ track, setTrack }) {
     return (
-      <header className="bg-base-200 py-10">
+      <header className="py-10">
           <div className="max-w-screen-md mx-auto text-center">
             <h1 className="text-5xl font-bold">Azure DevOps</h1>
             <p className="py-6">Flashcards to help with <a className="link link-primary" href={az900}>AZ-900</a>, <a className="link link-primary" href={az204}>AZ-204</a> and <a className="link link-primary" href={az400}>AZ-400</a>.</p>
