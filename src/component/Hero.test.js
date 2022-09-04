7f36eacedd038a7bade8af6821b0e0e560403aca
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-04 09:00:03 +0000

    test: hero

---
+++ src/component/Hero.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Hero from './Hero';
+
+test('renders w/o crashing', () => {
+  render(<Hero />);
+  const linkElement = screen.getByText(/Azure DevOps/i);
+  expect(linkElement).toBeInTheDocument();
+});
