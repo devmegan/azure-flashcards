9af14f06f6f34ab879c4b9e9ac7f5a99d117e9ed
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-07 16:24:42 +0000

    add: complete toast

---
+++ src/component/toast/Complete.js
@@ -0,0 +1,7 @@
+export default function Complete() {
+    return (
+        <div className="alert alert-success bg-green-800 shadow-lg mb-3 max-w-md flex justify-center mx-auto">
+            🥳 Success! You've correctly answered all the questions in this track.
+        </div> 
+    );
+}
