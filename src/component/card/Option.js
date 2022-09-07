e42fd01f458cee713624f10092de9a8337df072e
devmegan <megan.molloy@canalrivertrust.org.uk>
2022-09-07 14:10:45 +0000

    update: option radio name

--- src/component/card/Option.js
+++ src/component/card/Option.js
@@ -2,8 +2,8 @@
 export default function Option({ option }) {
     return (
         <label className="label cursor-pointer">
-            <span className="label-text text-md">{option}</span> 
-            <input type="radio" name={option} className="radio radio-primary" />
-        </label>
+        <span className="label-text">{option}</span> 
+        <input type="radio" name="options" className="radio radio-primary" />
+      </label>
     );
 }
